b71812f1f80da9b5e27cdfa16ea0c8b6bee7bfda
dmitry <ya.yarochkin@yandex.ru>
2022-03-18 18:46:44 +0300

    play array

--- sketch.js
+++ sketch.js
@@ -1,4 +1,20 @@
 let data = [100,440,500]
+let dataObg=[{
+    pos:10,
+    neg:30,
+    neit:50
+},
+{
+    pos:100,
+    neg:300,
+    neit:500
+},
+    {
+    pos:100,
+    neg:300,
+    neit:500
+},
+]
  function setup() {
   createCanvas(400, 400);
  }
@@ -6,11 +22,17 @@ function draw() {
   background(220);
 }
 
-let  playDATA = function(){
+let  playParam = function(DATA){
   var  wave;
   wave= new  p5.Oscillator('sine');
   wave.setType('sine')
   wave.amp(1);
-  wave.freq(300)
-  wave.start();  
+  wave.freq(DATA)
+  wave.start();
 }
+
+let playData = function (){
+    data.forEach((x)=>playParam(x))
+}
+
+
