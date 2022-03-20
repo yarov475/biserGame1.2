a261651f6f5590af5b708f44cecf4017e182393c
dmitry <ya.yarochkin@yandex.ru>
2022-03-20 14:10:55 +0300

    simple server fetch take object od int

--- server/server.js
+++ server/server.js
@@ -1,47 +1,48 @@
-var http = require('http');
+var fs = require('fs');
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
 }).listen(8080);
 console.log('server on 8080')
+var Sentiment = require('sentiment');
 
-
-
-// var    fs = require('fs');
-// import fetch from 'node-fetch'
-
-// const fetch = require('node-fetch');
+var sentiment = new Sentiment();
 const axios = require('axios').default;
 
-// axios.<method> will now provide autocomplete and parameter typings
+const URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=56ae5cd799ae474b9d52f79aa9905119';
 
-
-// fs.readFile('../index.html', function (err, html) {
-//     if (err) {
-//         throw err;
-//     }
-//     http.createServer(function(request, response) {
-//         response.writeHeader(200, {"Content-Type": "text/html"});
-//         response.write(html);
-//         response.end();
-//     }).listen(8000);
-// });
-
- const URL =  'https://newsapi.org/v2/top-headlines?country=us&apiKey=56ae5cd799ae474b9d52f79aa9905119';
 axios({
-  method: 'get',
-  url: URL,
+    method: 'get',
+    url: URL,
 })
-  .then(function (response) {
-     const objArr= response.data
-      console.log(objArr)
-})
+    .then(function (response) {
+            let list = [];
+            for (let i = 0; i < response.data.articles.length; i++) {
+                let news = list.push(sentiment.analyze(response.data.articles[i]['title'])['score'])
+
+            }
+            console.log(list, typeof list)
+
+            var val = [];
+            for (var key in list) {
+                if (list.hasOwnProperty(key)) {
+                    let number = list[key]
+                    val.push(number)
+                }
+            }
+            console.log(val, typeof val)
+        }
+    )
+// TODO get data from axios
+// TODO give sound to sent2sound
+// TODO visualise sent2sound
+// TODO make refresh
+let sent2sound = [
+    0, 3, -2, -2, -5, -6, 0,
+    -4, 1, 1, 4, -2, -5, 0,
+    0, 0, -1, 0, 4, 0
+]
+console.log(sent2sound, 'length: ', sent2sound.length, typeof sent2sound)
 
 
-
-
-var Sentiment = require('sentiment');
-var sentiment = new Sentiment();
-var result = sentiment.analyze('Cats are stupid.');
-console.dir(result);    // Score: -2, Comparative: -0.666
